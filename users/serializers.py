a1ca629d8823142c9b63ff2366994dafcd37a474
Taltal <taltalrealty@gmail.com>
2022-05-05 14:27:42 +0900

    2.12

--- users/serializers.py
+++ users/serializers.py
@@ -13,31 +13,60 @@ class RelatedUserSerializer(serializers.ModelSerializer):
             'superhost'
         )
 
-class ReadUserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        exclude = (
-            "groups",
-            "user_permissions",
-            "password",
-            "last_login",
-            "is_superuser",
-            "is_staff",
-            "is_active",
-            "favs",
-            "date_joined",
-        )
 
+class UserSerializer(serializers.ModelSerializer):
 
-class WriteUserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
     class Meta:
         model = User
         fields = (
+            'id',
             'username',
-            'first_name',
             'last_name',
-            'email'
+            'email',
+            'avatar',
+            'superhost',
+            'password',
+        )
+        read_only_fields = (
+            'id',
+            'superhost',
+            'avatar'
         )
 
-        def validate_first_name(self, value):
-            return value.upper()
+    def create(self, validated_data):
+        password = validated_data.get("password")
+        user = super().create(validated_data)
+        user.set_password(password)
+        user.save()
+        return user
+
+# class ReadUserSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = User
+#         exclude = (
+#             "groups",
+#             "user_permissions",
+#             "password",
+#             "last_login",
+#             "is_superuser",
+#             "is_staff",
+#             "is_active",
+#             "favs",
+#             "date_joined",
+#         )
+
+
+# class WriteUserSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = User
+#         fields = (
+#             'username',
+#             'first_name',
+#             'last_name',
+#             'email'
+#         )
+
+#         def validate_first_name(self, value):
+#             return value.upper()
--- users/views.py
+++ users/views.py
@@ -1,11 +1,14 @@
+import jwt
+from django.conf import settings
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from rest_framework import status
+from django.contrib.auth import authenticate
 from rooms.serializers import RoomSerializer
 from rooms.models import Room
-from .serializers import ReadUserSerializer, WriteUserSerializer
+from .serializers import UserSerializer
 from .models import User
 
 
@@ -14,9 +17,9 @@ class MeView(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
-        return Response(ReadUserSerializer(request.user).data)
+        return Response(UserSerializer(request.user).data)
     def put(self, request):
-        serializer = WriteUserSerializer(request.user, data=request.data, partial=True)
+        serializer = UserSerializer(request.user, data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
             return Response()
@@ -27,7 +30,7 @@ class MeView(APIView):
 def user_detail(request, pk):
     try:
         user = User.objects.get(pk=pk)
-        return Response(ReadUserSerializer(user).data)
+        return Response(UserSerializer(user).data)
     except User.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
 
@@ -56,3 +59,28 @@ class FavsView(APIView):
             except Room.DoesNotExist:
                 pass
         return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+class UsersView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            new_user = serializer.save()
+            return Response(UserSerializer(new_user).data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["POST"])
+def login(request):
+    username = request.data.get("username")
+    password = request.data.get("password")
+    if not username or not password:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        encoded_jwt = jwt.encode({"pk":user.pk}, settings.SECRET_KEY, algorithm="HS256")
+        print(encoded_jwt)
+        return Response(data={"token":encoded_jwt})
+    else:
+        return Response(status=status.HTTP_401_UNAUTHORIZED)
