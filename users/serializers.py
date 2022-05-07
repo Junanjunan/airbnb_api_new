e8a5c4dc07000b888f0308c6fc515ad08cddecbb
Taltal <taltalrealty@gmail.com>
2022-05-08 02:02:41 +0900

    .

--- users/serializers.py
+++ users/serializers.py
@@ -23,6 +23,7 @@ class UserSerializer(serializers.ModelSerializer):
         fields = (
             'id',
             'username',
+            'first_name',
             'last_name',
             'email',
             'avatar',
