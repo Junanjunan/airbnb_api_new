d5c7083f1a942a394faa4d76aab4d3675d7fc4e9
Taltal <taltalrealty@gmail.com>
2022-05-12 02:21:06 +0900

    .

--- users/urls.py
+++ users/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path("", views.UsersView.as_view()),
     path("login/", views.login),
     path("me/", views.MeView.as_view()),
-    path("me/favs", views.FavsView.as_view()),
+    path("me/favs/", views.FavsView.as_view()),
     path("<int:pk>/", views.user_detail),
-    path("<int:pk>/favs", views.UserFavsView.as_view())
+    path("<int:pk>/favs/", views.UserFavsView.as_view())
 ]
